d0f813c035357668f4a31e16d07ac63372771e78
Melanie MacDonald <macdon6m@udml7.local>
2018-11-08 14:57:16 -0600

    Initial Commit

---
+++ CheckTheList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckTheList
+//
+//  Created by Melanie MacDonald on 2018-11-08.
+//  Copyright © 2018 Melanie MacDonald. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
