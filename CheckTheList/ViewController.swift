cb9c4046fd8905c9dec84e4d056808295f1edef1
Kayvee <kv@github.com>
2018-11-21 19:21:37 -0600

    viewController

--- CheckTheList/ViewController.swift
+++ CheckTheList/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 import Firebase
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameFeild: UITextField!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+   let rootRef =  Database.database().reference()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+
+    @IBAction func sumbitButton(_ sender: Any) {
+        
+      
+       
+    }
 
 }
 
